061bd3ce38dec665c05923a819ef5d5ac390ff01
Huy <kingbazoka@gmail.com>
2017-11-22 19:17:23 -0800

    Colorized version

--- bubble.c
+++ bubble.c
@@ -2,10 +2,10 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define DATA_LENGTH  30
-#define IMAGE_WIDTH  300
-#define IMAGE_HEIGHT 50
-#define ENDING_DELAY 10
+#define DATA_LENGTH  100
+#define IMAGE_WIDTH  500
+#define IMAGE_HEIGHT 100
+#define ENDING_DELAY 50
 
 int* gen_data(int length) {
   int* data = (int*)malloc(sizeof(int) * length);
@@ -15,13 +15,19 @@ int* gen_data(int length) {
   return data;
 }
 
-void draw_frame(int* data, int length, int width, int height) {
+void draw_frame(int* data, int length, int width, int height, int highlight) {
   int block_width = width / length;
-  printf("P2\n%d %d\n255\n", width, height);
+  printf("P3\n%d %d\n255\n", width, height);
   for (int h = 0; h < height; h++) {
     for (int i = 0; i < length; i++) {
+      int r = data[i], g = data[i], b = data[i];
+      if (highlight == i) {
+        if (h <= block_width || h >= height - block_width) {
+          r = 255; g = b = 0;
+        }
+      }
       for (int c = 0; c < block_width; c++) {
-        printf("%3d ", data[i]);
+        printf("%d %d %d \t", r, g, b);
       }
     }
     printf("\n");
@@ -39,7 +45,7 @@ void bubble_sort(int* data, int length) {
     for (int j = 0; j < length - i; j++) {
       if (data[j] > data[j+1]) {
         swap(data, j, j+1);
-        draw_frame(data, length, IMAGE_WIDTH, IMAGE_HEIGHT);
+        draw_frame(data, length, IMAGE_WIDTH, IMAGE_HEIGHT, j+1);
       }
     }
   }
@@ -53,7 +59,7 @@ int main() {
   bubble_sort(inputs, DATA_LENGTH);
 
   for (int i = 0; i < ENDING_DELAY; i++) {
-    draw_frame(inputs, DATA_LENGTH, IMAGE_WIDTH, IMAGE_HEIGHT);
+    draw_frame(inputs, DATA_LENGTH, IMAGE_WIDTH, IMAGE_HEIGHT, -1);
   }
 
   return 0;
