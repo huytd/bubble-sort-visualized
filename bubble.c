3b5265688ea2f6ba0e51f625ab947584b4dcdaa3
Huy <kingbazoka@gmail.com>
2017-11-23 16:08:45 -0800

    Slow version with colored travel pointer. Check previous commit for faster version

--- bubble.c
+++ bubble.c
@@ -15,7 +15,7 @@ int* gen_data(int length) {
   return data;
 }
 
-void draw_frame(int* data, int length, int width, int height, int highlight) {
+void draw_frame(int* data, int length, int width, int height, int highlight, int second_highlight, int third_highlight) {
   int block_width = width / length;
   printf("P3\n%d %d\n255\n", width, height);
   for (int h = 0; h < height; h++) {
@@ -24,6 +24,16 @@ void draw_frame(int* data, int length, int width, int height, int highlight) {
       if (highlight == i) {
         if (h <= block_width || h >= height - block_width) {
           r = 255; g = b = 0;
+        } 
+      }
+      if (second_highlight == i) {
+        if (h <= block_width || h >= height - block_width) {
+          r = 255; g = 255; b = 0;
+        }
+      }
+      if (third_highlight == i) {
+        if (h <= block_width || h >= height - block_width) {
+          r = 0; g = 0; b = 255;
         }
       }
       for (int c = 0; c < block_width; c++) {
@@ -43,9 +53,15 @@ void swap(int* data, int from, int to) {
 void bubble_sort(int* data, int length) {
   for (int i = 0; i < length; i++) {
     for (int j = 0; j < length - i; j++) {
+      draw_frame(data, length, IMAGE_WIDTH, IMAGE_HEIGHT, -1, -1, j);
       if (data[j] > data[j+1]) {
+        draw_frame(data, length, IMAGE_WIDTH, IMAGE_HEIGHT, j, j+1, -1);
+        draw_frame(data, length, IMAGE_WIDTH, IMAGE_HEIGHT, j, j+1, -1);
+        draw_frame(data, length, IMAGE_WIDTH, IMAGE_HEIGHT, j, j+1, -1);
         swap(data, j, j+1);
-        draw_frame(data, length, IMAGE_WIDTH, IMAGE_HEIGHT, j+1);
+        draw_frame(data, length, IMAGE_WIDTH, IMAGE_HEIGHT, j+1, j, -1);
+        draw_frame(data, length, IMAGE_WIDTH, IMAGE_HEIGHT, j+1, j, -1);
+        draw_frame(data, length, IMAGE_WIDTH, IMAGE_HEIGHT, j+1, j, -1);
       }
     }
   }
@@ -59,7 +75,7 @@ int main() {
   bubble_sort(inputs, DATA_LENGTH);
 
   for (int i = 0; i < ENDING_DELAY; i++) {
-    draw_frame(inputs, DATA_LENGTH, IMAGE_WIDTH, IMAGE_HEIGHT, -1);
+    draw_frame(inputs, DATA_LENGTH, IMAGE_WIDTH, IMAGE_HEIGHT, -1, -1, -1);
   }
 
   return 0;
